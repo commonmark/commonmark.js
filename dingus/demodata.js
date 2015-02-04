3acf8a5b06723d08f8ba13567f033e42c246fcc3
John MacFarlane <jgm@berkeley.edu>
2015-02-03 20:09:49 -0800

    Initial commit of files to create dingus.
    Borrowed from markdown-it.

---
+++ dingus/demodata.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// Build demo data for embedding into html
+
+/*eslint no-console:0*/
+
+var fs   = require('fs');
+var path = require('path');
+
+console.log(JSON.stringify({
+  self: {
+    demo: {
+      source: fs.readFileSync(path.join(__dirname, './sample.md'), 'utf8')
+    }
+  }
+}, null, 2));
