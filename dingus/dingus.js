b55cba44d29f53b1e1e492b0bc9514f9190b6bdc
John MacFarlane <jgm@berkeley.edu>
2015-04-04 22:18:29 -0700

    Dingus: reindented.

--- dingus/dingus.js
+++ dingus/dingus.js
@@ -9,96 +9,96 @@ var xmlwriter = new commonmark.XmlRenderer({ sourcepos: true });
 var reader = new commonmark.Parser();
 
 function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] === variable){
-      return decodeURIComponent(pair[1]);
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === variable){
+            return decodeURIComponent(pair[1]);
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 $(document).ready(function() {
-  var x;
-  var textarea = $("#text");
-  var render = function(parsed) {
-    if (parsed === undefined) {
-      return;
-    }
-    var startTime = new Date().getTime();
-    var result = writer.render(parsed);
-    var endTime = new Date().getTime();
-    var renderTime = endTime - startTime;
-    $("#preview").html(result);
-    $("#html").text(result);
-    $("#ast").text(xmlwriter.render(parsed));
-    $("#rendertime").text(renderTime);
-  };
-  var syncScroll = function() {
-    var lineHeight = parseFloat(textarea.css('line-height'));
-    // TODO this ignores the fact that lines can wrap - fix!
-    var lineNumber = Math.floor(textarea.scrollTop() / lineHeight) + 1;
-    var elt = $("#preview [data-sourcepos^='" + lineNumber + ":']").last();
-    if (elt.length > 0) {
-        if (elt.offset()) {
-            var curTop = $("#preview").scrollTop();
-            $("#preview").animate({
-                scrollTop: curTop + elt.offset().top - 100
-            }, 50);
+    var x;
+    var textarea = $("#text");
+    var render = function(parsed) {
+        if (parsed === undefined) {
+            return;
         }
-    }
-  };
-  var markSelection = function() {
-    var cursorPos = $("#text").prop("selectionStart");
-    // now count newline up to this pos
-    var textval = $("#text").val();
-    var lineNumber = 1;
-    for (var i = 0; i < cursorPos; i++) {
-        if (textval.charAt(i) === '\n') {
-            lineNumber++;
+        var startTime = new Date().getTime();
+        var result = writer.render(parsed);
+        var endTime = new Date().getTime();
+        var renderTime = endTime - startTime;
+        $("#preview").html(result);
+        $("#html").text(result);
+        $("#ast").text(xmlwriter.render(parsed));
+        $("#rendertime").text(renderTime);
+    };
+    var syncScroll = function() {
+        var lineHeight = parseFloat(textarea.css('line-height'));
+        // TODO this ignores the fact that lines can wrap - fix!
+        var lineNumber = Math.floor(textarea.scrollTop() / lineHeight) + 1;
+        var elt = $("#preview [data-sourcepos^='" + lineNumber + ":']").last();
+        if (elt.length > 0) {
+            if (elt.offset()) {
+                var curTop = $("#preview").scrollTop();
+                $("#preview").animate({
+                    scrollTop: curTop + elt.offset().top - 100
+                }, 50);
+            }
         }
+    };
+    var markSelection = function() {
+        var cursorPos = $("#text").prop("selectionStart");
+        // now count newline up to this pos
+        var textval = $("#text").val();
+        var lineNumber = 1;
+        for (var i = 0; i < cursorPos; i++) {
+            if (textval.charAt(i) === '\n') {
+                lineNumber++;
+            }
+        }
+        var elt = $("#preview [data-sourcepos^='" + lineNumber + ":']").last();
+        if (elt.length > 0) {
+            $("#preview .selected").removeClass("selected");
+            elt.addClass("selected");
+            syncScroll();
+        }
+    };
+    var parseAndRender = function() {
+        if (x) { x.abort(); } // If there is an existing XHR, abort it.
+        var startTime = new Date().getTime();
+        var parsed = reader.parse(textarea.val());
+        var endTime = new Date().getTime();
+        var parseTime = endTime - startTime;
+        $("#parsetime").text(parseTime);
+        $(".timing").css('visibility', 'visible');
+        render(parsed);
+        markSelection();
+    };
+    var initial_text = getQueryVariable("text");
+    if (initial_text) {
+        textarea.val(initial_text);
+        // show HTML tab if text is from query
+        $('#result-tabs a[href="#result"]').tab('show');
     }
-    var elt = $("#preview [data-sourcepos^='" + lineNumber + ":']").last();
-    if (elt.length > 0) {
-        $("#preview .selected").removeClass("selected");
-        elt.addClass("selected");
-        syncScroll();
-    }
-  };
-  var parseAndRender = function() {
-    if (x) { x.abort(); } // If there is an existing XHR, abort it.
-    var startTime = new Date().getTime();
-    var parsed = reader.parse(textarea.val());
-    var endTime = new Date().getTime();
-    var parseTime = endTime - startTime;
-    $("#parsetime").text(parseTime);
-    $(".timing").css('visibility', 'visible');
-    render(parsed);
-    markSelection();
-  };
-  var initial_text = getQueryVariable("text");
-  if (initial_text) {
-    textarea.val(initial_text);
-    // show HTML tab if text is from query
-    $('#result-tabs a[href="#result"]').tab('show');
-  }
 
-  parseAndRender();
-  $("#clear-text-box").click(function() {
-    textarea.val('');
     parseAndRender();
-  });
-  $("#permalink").click(function() {
-    window.location.pathname = "/index.html";
-    window.location.search = "text=" + encodeURIComponent(textarea.val());
-  });
-  textarea.bind('input propertychange', _.debounce(parseAndRender, 50, { maxWait: 100 }));
-  textarea.on('scroll', _.debounce(syncScroll, 50, { maxWait: 50 }));
-  textarea.on('keydown click focus', _.debounce(markSelection, 50, { maxWait: 100}));
-  $("#smart").click(function() {
-      reader = new commonmark.Parser({smart: $("#smart").is(":checked")});
-      parseAndRender();
-  });
+    $("#clear-text-box").click(function() {
+        textarea.val('');
+        parseAndRender();
+    });
+    $("#permalink").click(function() {
+        window.location.pathname = "/index.html";
+        window.location.search = "text=" + encodeURIComponent(textarea.val());
+    });
+    textarea.bind('input propertychange', _.debounce(parseAndRender, 50, { maxWait: 100 }));
+    textarea.on('scroll', _.debounce(syncScroll, 50, { maxWait: 50 }));
+    textarea.on('keydown click focus', _.debounce(markSelection, 50, { maxWait: 100}));
+    $("#smart").click(function() {
+        reader = new commonmark.Parser({smart: $("#smart").is(":checked")});
+        parseAndRender();
+    });
 });
