8b6ce99dd6579873baec46e6fdac58778d095285
John MacFarlane <jgm@berkeley.edu>
2020-12-05 09:43:30 -0800

    Use rollup `--banner` to include license info.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { uglify } from "rollup-plugin-uglify";
+import { version } from './package.json';
+
+var banner = "/* commonmark " + version + " https://github.com/commonmark/commonmark.js @license BSD3 */";
 
 export default {
     input: "lib/index.js",
@@ -10,12 +13,14 @@ export default {
       {
         file: "dist/commonmark.js",
         format: "umd",
-        name: "commonmark"
+        name: "commonmark",
+        banner: banner,
       },
       {
         file: "dist/commonmark.min.js",
         format: "umd",
         name: "commonmark",
+        banner: banner,
         plugins: [uglify()]
       }
     ],
